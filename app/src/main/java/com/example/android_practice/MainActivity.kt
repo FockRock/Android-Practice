fe56a481db63ba370682cb5ca57fef8a5ba24444
FockRock <smirnov_nikita@mail.ru>
2024-04-03 21:21:42 +0300

    Text changed

--- app/src/main/java/com/example/android_practice/MainActivity.kt
+++ app/src/main/java/com/example/android_practice/MainActivity.kt
@@ -1,22 +1,21 @@
 package com.example.android_practice
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.android_practice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bindingClass: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bindingClass = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bindingClass.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        bindingClass.bPushMe.setOnClickListener {                                            //по нажатию на кнопку меняется надпись и размер текста
-            bindingClass.tSatisfaction.text = getString(R.string.satisfaction)
-            bindingClass.tSatisfaction.textSize = 65F
+        binding.tSatisfaction.text = getString(R.string.push_button)
+
+        binding.bPushMe.setOnClickListener {                                            //по нажатию на кнопку меняется надпись и размер текста
+            binding.tSatisfaction.text = getString(R.string.satisfaction)
+            binding.tSatisfaction.textSize = 65F
         }
     }
 
